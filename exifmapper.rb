f074bab9d27cdae6eb8dd430631ed5fe8a607620
sircryptic <48811414+SirCryptic@users.noreply.github.com>
2023-01-13 11:12:02 +0000

    Update exifmapper.rb

--- exifmapper.rb
+++ exifmapper.rb
@@ -2,28 +2,36 @@
 ##
 ## feel free to try fix my jank ass code
 ##
-def get_gps_from_exif file
-  `exiftool -c "%.6f" #{file} | grep GPS | grep Position`.scan(/(\d+\.\d+)/);
-end
+require 'exifr'
 
-zoom = 2;
-path = "*.jpg";
-map_url = "https://staticmap.openstreetmap.de/staticmap.php?&zoom=#{zoom}&size=865x512&maptype=mapnik&markers=" ;
-all = Dir.glob(path);
-total = all.count
-has_gps = 0;
-meta_exif = 0;
-all.each do |file|
-  if gps = get_gps_from_exif(file)
-    if gps.count==2 # lat and long
-      coord = "#{gps[0][0]},#{gps[1][0]}"
-      puts "=> #{file} @ #{coord}"
-      map_url += "#{coord},lightblue3#{file}"
-      meta_exif+=1
-      has_gps+=1
-    end
+def get_gps_coordinates(file)
+  exif = EXIFR::JPEG.new(file)
+  if exif.gps
+    [exif.gps.latitude, exif.gps.longitude]
+  else
+    nil
   end
 end
-puts "=> Total #{total} images | #{meta_exif} with EXIF | #{has_gps} with location";
-puts ("=> Percentage with location = %3.2f" % [(has_gps*100).to_f/total]);
+
+zoom = 2
+path = "*.jpg"
+map_url = "https://staticmap.openstreetmap.de/staticmap.php?&zoom=#{zoom}&size=865x512&maptype=mapnik&markers="
+all_files = Dir.glob(path)
+total_files = all_files.count
+has_gps = 0
+meta_exif = 0
+
+all_files.each do |file|
+  gps = get_gps_coordinates(file)
+  if gps
+    coord = "#{gps[0]},#{gps[1]}"
+    puts "=> #{file} @ #{coord}"
+    map_url += "#{coord},lightblue3#{file}"
+    meta_exif += 1
+    has_gps += 1
+  end
+end
+
+puts "=> Total #{total_files} images | #{meta_exif} with EXIF | #{has_gps} with location"
+puts "=> Percentage with location = %3.2f" % [(has_gps*100.0/total_files)]
 puts "=> Map URL: #{map_url}"
